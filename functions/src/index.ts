edeb4de6a15dec0149f9cedd796081728d8e6bbe
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2023-09-22 17:08:47 +0200

    env vars

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,16 +1,12 @@
 import {onCall} from "firebase-functions/v2/https";
-import {defineSecret} from "firebase-functions/params";
 import {Client} from "@notionhq/client";
-
-const NOTION_SECRET = defineSecret("NOTION_SECRET");
-const NOTION_PAGE_ID = defineSecret("NOTION_PAGE_ID");
 
 exports.getNotionPage = onCall(async () => {
   const notion = new Client({
-    auth: NOTION_SECRET.value(),
+    auth: process.env.NOTION_SECRET,
   });
 
   return await notion.pages.retrieve({
-    page_id: NOTION_PAGE_ID.value(),
+    page_id: process.env.NOTION_PAGE_ID || "",
   });
 });
