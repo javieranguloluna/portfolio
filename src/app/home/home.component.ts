b2c732bf00c0d28029a966795572a844a86ac723
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2023-09-22 16:23:19 +0200

    notion function test

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Functions, httpsCallable } from '@angular/fire/functions';
 
 @Component({
   selector: 'app-home',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+  private getNotionPage
+
+  constructor(functions: Functions) {
+    this.getNotionPage = httpsCallable(functions, 'getNotionPage')
+
+  }
+  
+  public async notion() {
+    const n = await this.getNotionPage()
+    console.log(n)
+  }
 }
