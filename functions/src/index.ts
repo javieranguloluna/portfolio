b91a8f5ac9396c56e984248526eb76adbde8d92b
javieranguloluna <54935581+javieranguloluna@users.noreply.github.com>
2023-09-22 16:10:47 +0200

    reconfig firebase functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,16 @@
 import {onCall} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
 import {defineSecret} from "firebase-functions/params";
-
 import {Client} from "@notionhq/client";
 
 const NOTION_SECRET = defineSecret("NOTION_SECRET");
 const NOTION_PAGE_ID = defineSecret("NOTION_PAGE_ID");
 
-const notion = new Client({
-  auth: NOTION_SECRET.value(),
-});
+exports.getNotionPage = onCall(async () => {
+  const notion = new Client({
+    auth: NOTION_SECRET.value(),
+  });
 
-exports.getNotionPage = onCall(() => {
-  return notion.pages.retrieve({
+  return await notion.pages.retrieve({
     page_id: NOTION_PAGE_ID.value(),
   });
 });
