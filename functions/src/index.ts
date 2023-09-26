f7074eec103115561541c279a9282f67d4343433
JAL <javier.angulo.luna@gmail.com>
2023-09-26 14:02:25 +0200

    main branch sync

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,64 @@
-import {onCall} from "firebase-functions/v2/https";
-// import * as logger from "firebase-functions/logger";
-import {defineSecret} from "firebase-functions/params";
-
+import {CallableRequest, onCall} from "firebase-functions/v2/https";
 import {Client} from "@notionhq/client";
 
-const NOTION_SECRET = defineSecret("NOTION_SECRET");
-const NOTION_PAGE_ID = defineSecret("NOTION_PAGE_ID");
+interface Contact {
+  name: string;
+  email: string;
+  phone: string;
+  contactPreference: string;
+  message: string;
+}
 
-const notion = new Client({
-  auth: NOTION_SECRET.value(),
+exports.getNotionPage = onCall(async () => {
+  const notion = new Client({
+    auth: process.env.NOTION_SECRET,
+  });
+
+  const page = await notion.pages.retrieve({
+    page_id: process.env.NOTION_PAGE_ID || "",
+  });
+  const blocks = await notion.blocks.children.list({
+    block_id: process.env.NOTION_PAGE_ID || "",
+  });
+
+  return {page, blocks};
 });
 
-exports.getNotionPage = onCall(() => {
-  return notion.pages.retrieve({
-    page_id: NOTION_PAGE_ID.value(),
+exports.submitContact = onCall(async (request: CallableRequest<Contact>) => {
+  const notion = new Client({
+    auth: process.env.NOTION_SECRET,
   });
+
+  const create = await notion.pages.create({
+    parent: {
+      type: "database_id",
+      database_id: process.env.NOTION_DATABASE_ID || ""
+    },
+    properties: {
+      Name: {
+        type: "title",
+        title: [{ type: "text", text: { content: request.data.name } }]
+      },
+      Email: {
+        type: "email",
+        email: request.data.email
+      },
+      Phone: {
+        type: "phone_number",
+        phone_number: request.data.phone 
+      },
+      "Contact Preference": {
+        type: "select",
+        select: {
+          name: request.data.contactPreference
+        }
+      },
+      Message: {
+        type: "rich_text",
+        rich_text: [{ text: { content: request.data.message } }]
+      }
+    }
+  })
+
+  return create;
 });
